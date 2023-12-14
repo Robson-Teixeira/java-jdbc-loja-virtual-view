86749ede1e159cf24f4a71ea0848795c9ed758a9
Robson-Teixeira <robsonmteixeira@outlook.com>
2023-12-14 01:08:24 -0300

    Aplicação Desktop: Implementando as controllers

--- src/br/com/alura/jdbc/controller/CategoriaController.java
+++ src/br/com/alura/jdbc/controller/CategoriaController.java
@@ -1,16 +1,21 @@
 package br.com.alura.jdbc.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import br.com.alura.jdbc.dao.CategoriaDAO;
+import br.com.alura.jdbc.factory.ConnectionFactory;
 import br.com.alura.jdbc.modelo.Categoria;
 
 public class CategoriaController {
 
+	CategoriaDAO categoriaDao;
+
+	public CategoriaController() {
+		this.categoriaDao = new CategoriaDAO(
+				new ConnectionFactory().recuperarConexao());
+	}
+
 	public List<Categoria> listar() {
-		List<Categoria> categorias = 
-				new ArrayList<Categoria>();
-		categorias.add(new Categoria(1, "Categoria de teste")); 
-		return categorias;
+		return this.categoriaDao.listar();
 	}
 }
--- src/br/com/alura/jdbc/controller/ProdutoController.java
+++ src/br/com/alura/jdbc/controller/ProdutoController.java
@@ -1,29 +1,33 @@
 package br.com.alura.jdbc.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import br.com.alura.jdbc.dao.ProdutoDAO;
+import br.com.alura.jdbc.factory.ConnectionFactory;
 import br.com.alura.jdbc.modelo.Produto;
 
 public class ProdutoController {
+	
+	ProdutoDAO produtoDao;
+	
+	public ProdutoController() {
+		this.produtoDao = new ProdutoDAO(
+				new ConnectionFactory().recuperarConexao());
+	}
 
 	public void deletar(Integer id) {
-		System.out.println("Deletando produto");
+		this.produtoDao.deletar(id);
 	}
 
 	public void salvar(Produto produto) {
-		System.out.println("Salvando produto");
+		this.produtoDao.salvar(produto);
 	}
 
 	public List<Produto> listar() {
-		List<Produto> produtos = 
-				new ArrayList<Produto>();
-		produtos.add(new Produto("Nome do Produto de teste"
-				, "Descrição do produto de teste"));
-		return produtos;
+		return this.produtoDao.listar();
 	}
 
 	public void alterar(String nome, String descricao, Integer id) {
-		System.out.println("Alterando produto");
+		this.produtoDao.alterar(nome, descricao, id);
 	}
 }
